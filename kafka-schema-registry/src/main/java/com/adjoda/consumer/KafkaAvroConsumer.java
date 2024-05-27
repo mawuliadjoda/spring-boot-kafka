72e65c2b111ca413fa1f0d91d1f8d33600f3e309
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-05-28 00:22:08 +0200

    Spring Boot | Kafka Schema Registry & Avro with Practical Example and Implementation

---
+++ kafka-schema-registry/src/main/java/com/adjoda/consumer/KafkaAvroConsumer.java
@@ -0,0 +1,20 @@
+package com.adjoda.consumer;
+
+
+import com.adjoda.dto.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaAvroConsumer {
+
+    @KafkaListener(topics = "${topic.name}")
+    public void read(ConsumerRecord<String, Employee> consumerRecord) {
+        String key = consumerRecord.key();
+        Employee employee = consumerRecord.value();
+        log.info("Avro message received for key : " + key + " Value : " +  employee.toString());
+    }
+}
---
+++ kafka-schema-registry/src/main/java/com/adjoda/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.adjoda.controller;
+
+import com.adjoda.dto.Employee;
+import com.adjoda.producer.KafkaAvroProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+    @Autowired
+    private final KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+
+}
